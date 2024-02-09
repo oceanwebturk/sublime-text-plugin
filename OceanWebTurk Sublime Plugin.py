e8af1dac9e80636e65b093db41c1a2b64c57d1e6
Mehmet Ali Durusoy <mehmetalidurusoy73+github@gmail.com>
2024-02-09 18:59:53 +0300

    Create Settings and Python file updated

--- OceanWebTurk Sublime Plugin.py
+++ OceanWebTurk Sublime Plugin.py
@@ -1,2 +1,12 @@
+import os
+import shlex
+import subprocess
 import sublime
 import sublime_plugin
+
+class OceanWebTurk():
+    def __init__(self, *arg):
+        
+
+    def run():
+        print("Coder")
